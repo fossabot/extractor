dd431c82998e44d3423b0bf9b6691efd3df660b9
Mya Pitzeruse <j.mya.pitz@gmail.com>
2019-03-01 18:47:02 -0600

    nobug: optimized imports and fixed up typings

--- api/extractor.d.ts
+++ api/extractor.d.ts
@@ -1,13 +1,13 @@
-import { DependencyManagementFile } from "./deps"
-import { Client, ServerUnaryCall, ClientUnaryCall, ServiceDefinition, ChannelCredentials } from "grpc";
+import {ChannelCredentials, Client, ServerUnaryCall, ServiceDefinition} from "grpc";
+import {DependencyManagementFile} from "./deps";
 
 export interface ExtractRequest {
-    repositoryUrl: string
+    repositoryUrl: string;
 }
 
 export interface ExtractResponse {
-    repositoryUrl: string
-    managementFiles: DependencyManagementFile[]
+    repositoryUrl: string;
+    managementFiles: DependencyManagementFile[];
 }
 
 export interface IDependencyExtractor {
@@ -15,9 +15,9 @@ export interface IDependencyExtractor {
 }
 
 export class DependencyExtractor extends Client {
-    static service: ServiceDefinition<IDependencyExtractor>;
+    public static service: ServiceDefinition<IDependencyExtractor>;
 
     constructor(address: string, credentials: ChannelCredentials, options?: object);
 
-    extract(request: ExtractRequest, callback: (error: Error, response: ExtractResponse) => void): void;
+    public extract(request: ExtractRequest, callback: (error: Error, response: ExtractResponse) => void): void;
 }
